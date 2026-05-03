d721349f2aa2fcf46b62e21b2d73cacf75febf33
Cristian Paniagua <tenondecrpc@gmail.com>
2026-05-03 20:45:52 -0300

    feat: improve chromadb isolated example

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import chromadb
+
 def main():
-    print("Hello from chromadb-rag!")
+    chroma_client = chromadb.Client()
+    collection = chroma_client.get_or_create_collection(name="my_collection")
+
+    collection.add(
+        ids=["id1", "id2"],
+        documents=[
+            "Python is a high-level programming language used for web development, data science, and automation",
+            "Oranges are a type of citrus fruit that are round, orange in color, and rich in vitamin C",
+        ],
+    )
+
+    results = collection.query(
+        query_texts=["programming languages for building websites"],
+        n_results=2,
+    )
+
+    print(results)
 
 
 if __name__ == "__main__":
